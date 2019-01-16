2b9dccea49374d8f5dcbb2e9e17d62d28cc3a448
yeyoan <yeyoan@gmail.com>
2019-01-16 11:19:01 +0800

    Fix hasSoft17 method

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -1,12 +1,25 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealer extends Player {
 
     /**
-     * Checks if this {@code Dealer} has a soft 17.
-     * @return true if this {@code Dealer}'s hand value is 17 and they have an {@link Ace} 
+     * Determines if this {@code Dealer} has a soft 17. A hand that contains an
+     * {@code Ace} counted as 11 is a soft hand. If the hand is soft and has a
+     * value of 17, it is a soft 17.
+     * @return true if the hand has a value of 17 and they have an {@code Ace} 
+     * counted as 11
      */
     public boolean hasSoft17() {
-        return hasAce() && getHandValue() == 17;
+        if (getHandValue() == 17 && hasAce()) {
+            int total = 0;
+            for (Card card : getHand()) {
+                total += (card instanceof Ace) ? 0 : card.getRank();
+            }
+            return total + 11 == 17 || total == 0;
+        }
+        return false;
     }
 }
